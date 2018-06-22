jahurley/nfp-drv-kmods

commit b87dba42b498ad8274724f2070f8f96edfd5fb05

nfp: initialize IPsec only if supported by firmware
Use the vNIC extended capabilities to determine whether IPsec offload is
available by the firmware.

Only initialize the IPsec component on a particular netdev if it's
supported by firmware. If it is, then enable the ESP hardware offload
feature.

We support this on both representors and vNICs.

Note that for representors to support IPsec offload, the underlying vNIC
also needs to support this.

Signed-off-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>

src/nfp_net_common.c
@@ -3972,6 +3972,15 @@ static void nfp_net_netdev_init(struct nfp_net *nn)
 		netdev->hw_features |= NETIF_F_HW_VLAN_CTAG_FILTER;
 		nn->dp.ctrl |= NFP_NET_CFG_CTRL_CTAG_FILTER;
 	}
+	if (nn->tlv_caps.extended_caps & NFP_NET_CFG_CTRL_EXT_IPSEC) {
+		if (nfp_net_ipsec_init(netdev)) {
+			nn_warn(nn,
+				"Unable to enable IPsec offload on device.\n");
+		} else {
+			netdev->hw_features |= NETIF_F_HW_ESP;
+			netdev->hw_enc_features |= NETIF_F_HW_ESP;
+		}
+	}
 
 	netdev->features = netdev->hw_features;
 
@@ -3985,8 +3994,6 @@ static void nfp_net_netdev_init(struct nfp_net *nn)
 	/* Finalise the netdev setup */
 	netdev->netdev_ops = &nfp_net_netdev_ops;
 	netdev->watchdog_timeo = msecs_to_jiffies(5 * 1000);
-
-	nfp_net_ipsec_init(netdev);
 
 	SWITCHDEV_SET_OPS(netdev, &nfp_port_switchdev_ops);
 

src/nfp_net_repr.c
@@ -408,9 +408,17 @@ int nfp_repr_init(struct nfp_app *app, struct net_device *netdev,
 	if (nn->cap & NFP_NET_CFG_CTRL_TXCSUM) {
 		netdev->hw_features |= NETIF_F_IP_CSUM | NETIF_F_IPV6_CSUM;
 	}
+	if (nn->tlv_caps.extended_caps & NFP_NET_CFG_CTRL_EXT_IPSEC) {
+		if (!(pf_netdev->hw_features & NETIF_F_HW_ESP) ||
+		    nfp_net_ipsec_init(netdev)) {
+			nn_warn(nn, "Unable to enable IPsec offload on repr device %s.\n",
+				netdev->name);
+		} else {
+			netdev->hw_features |= NETIF_F_HW_ESP;
+			netdev->hw_enc_features |= NETIF_F_HW_ESP;
+		}
+	}
 	netdev->features = netdev->hw_features;
-
-	nfp_net_ipsec_init(netdev);
 
 	err = nfp_app_repr_init(app, netdev);
 	if (err)

src/nfp_net_repr.h
@@ -117,7 +117,7 @@ enum nfp_repr_type {
 #define NFP_REPR_LINKED_NETIF_F					\
 	(NETIF_F_RXCSUM | NETIF_F_IP_CSUM | NETIF_F_IPV6_CSUM |	\
 	 NETIF_F_HW_VLAN_CTAG_RX | NETIF_F_HW_VLAN_CTAG_TX |	\
-	 NETIF_F_HW_VLAN_CTAG_FILTER)
+	 NETIF_F_HW_VLAN_CTAG_FILTER | NETIF_F_HW_ESP)
 
 extern const struct net_device_ops nfp_repr_netdev_ops;
 

commit e76b88b2215713d32a422dd1f4a60e762359b4a2

nfp: set NETIF_F_HW_ESP only if IPsec offloading available
Only attempt to set the NETIF_F_HW_ESP capability when we have
CONFIG_NFP_NET_IPSEC enabled. This fixes the case where the kernel
doesn't support ESP offload, but the driver requests this feature,
causing the register_netdev call to fail with EPERM.

Fixes: b87dba42: ("nfp: initialize IPsec only if supported by
firmware")
Signed-off-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>

src/nfp_net_common.c
@@ -4027,6 +4027,7 @@ static void nfp_net_netdev_init(struct nfp_net *nn)
 		netdev->hw_features |= NETIF_F_HW_VLAN_CTAG_FILTER;
 		nn->dp.ctrl |= NFP_NET_CFG_CTRL_CTAG_FILTER;
 	}
+#ifdef CONFIG_NFP_NET_IPSEC
 	if (nn->tlv_caps.extended_caps & NFP_NET_CFG_CTRL_EXT_IPSEC) {
 		if (nfp_net_ipsec_init(netdev)) {
 			nn_warn(nn,
@@ -4036,6 +4037,7 @@ static void nfp_net_netdev_init(struct nfp_net *nn)
 			netdev->hw_enc_features |= NETIF_F_HW_ESP;
 		}
 	}
+#endif
 
 	netdev->features = netdev->hw_features;
 

src/nfp_net_repr.c
@@ -409,6 +409,7 @@ int nfp_repr_init(struct nfp_app *app, struct net_device *netdev,
 	if (nn->cap & NFP_NET_CFG_CTRL_TXCSUM) {
 		netdev->hw_features |= NETIF_F_IP_CSUM | NETIF_F_IPV6_CSUM;
 	}
+#ifdef CONFIG_NFP_NET_IPSEC
 	if (nn->tlv_caps.extended_caps & NFP_NET_CFG_CTRL_EXT_IPSEC) {
 		if (!(pf_netdev->hw_features & NETIF_F_HW_ESP) ||
 		    nfp_net_ipsec_init(netdev)) {
@@ -419,6 +420,7 @@ int nfp_repr_init(struct nfp_app *app, struct net_device *netdev,
 			netdev->hw_enc_features |= NETIF_F_HW_ESP;
 		}
 	}
+#endif
 	netdev->features = netdev->hw_features;
 
 	err = nfp_app_repr_init(app, netdev);