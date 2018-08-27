jahurley/nfp-drv-kmods

commit 14d00eb54572e9b55e234ec889df591ba863a984

nfp: mbl: add repr_xmit callback for the ual
This provides the UAL the ability to get a callback for each packet that
gets transmitted via a representor. The UAL can take some action if it
desires at this point.

The main purpose of this callback is for the UAL to be able to direct
traffic to vNICs on a per-packet basis, e.g. flow affinity to a
particular vNIC.

The precedence is, UAL gets first option on setting the destination
vNIC, then any preconfigured VLAN destinations and finally the default
vNIC is selected for the representor.

Signed-off-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>

src/mbl/main.c
@@ -933,15 +933,24 @@ nfp_mbl_repr_xmit(struct nfp_app *app, struct sk_buff *skb,
 	struct nfp_mbl_repr *repr_priv = repr->app_priv;
 	u16 vid;
 
+	if (ctx->ual_ops && ctx->ual_ops->repr_xmit) {
+		err = ctx->ual_ops->repr_xmit(&ctx->ual_cookie, skb, repr);
+		if (err)
+			return err;
+	}
+
 	if (!skb_vlan_tag_present(skb))
 		return 0;
 
-	vid = skb_vlan_tag_get(skb);
+	/* Only update the destination if it hasn't been done by the UAL yet */
+	if (nfp_netdev_is_nfp_repr(skb->dev)) {
+		vid = skb_vlan_tag_get(skb);
 
-	skb_dst_drop(skb);
-	dst_hold((struct dst_entry *)repr_priv->vlan_dst[vid]);
-	skb_dst_set(skb, (struct dst_entry *)repr_priv->vlan_dst[vid]);
-	skb->dev = repr->dst->u.port_info.lower_dev;
+		skb_dst_drop(skb);
+		dst_hold((struct dst_entry *)repr_priv->vlan_dst[vid]);
+		skb_dst_set(skb, (struct dst_entry *)repr_priv->vlan_dst[vid]);
+		skb->dev = repr->dst->u.port_info.lower_dev;
+	}
 
 	return 0;
 }

src/mbl/nfp_ual.h
@@ -180,6 +180,9 @@ struct nfp_ual_ops {
 			     struct nfp_meta_parsed *meta);
 	int (*prep_tx_meta)(void *cookie, struct sk_buff *skb);
 
+	int (*repr_xmit)(void *cookie, struct sk_buff *skb,
+			 struct nfp_repr *repr);
+
 	void (*ctrl_msg_rx)(void *cookie, struct sk_buff *skb);
 
 	int (*sriov_enable)(void *cookie, struct nfp_mbl_dev_ctx *ctx,

src/nfp_net_repr.c
@@ -228,14 +228,17 @@ static netdev_tx_t nfp_repr_xmit(struct sk_buff *skb, struct net_device *netdev)
 	unsigned int len = skb->len;
 	int ret;
 
-	skb_dst_drop(skb);
-	dst_hold((struct dst_entry *)repr->dst);
-	skb_dst_set(skb, (struct dst_entry *)repr->dst);
-	skb->dev = repr->dst->u.port_info.lower_dev;
-
 	ret = nfp_app_repr_xmit(repr->app, skb, repr);
 	if (unlikely(ret))
 		return ret;
+
+	/* Only update the destination if it hasn't been done by app yet */
+	if (likely(nfp_netdev_is_nfp_repr(skb->dev))) {
+		skb_dst_drop(skb);
+		dst_hold((struct dst_entry *)repr->dst);
+		skb_dst_set(skb, (struct dst_entry *)repr->dst);
+		skb->dev = repr->dst->u.port_info.lower_dev;
+	}
 
 	ret = dev_queue_xmit(skb);
 	nfp_repr_inc_tx_stats(netdev, len, ret);

commit a2d8115544a9553d5988f6f804dc9f52b86bccfd

nfp: mbl: add missing variable declaration
Missed this addition due to a failed rebase for the original patch.

Fixes: 14d00eb5: ("nfp: mbl: add repr_xmit callback for the ual")
Signed-off-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>

src/mbl/main.c
@@ -932,6 +932,7 @@ nfp_mbl_repr_xmit(struct nfp_app *app, struct sk_buff *skb,
 {
 	struct nfp_mbl_repr *repr_priv = repr->app_priv;
 	u16 vid;
+	int err;
 
 	if (ctx->ual_ops && ctx->ual_ops->repr_xmit) {
 		err = ctx->ual_ops->repr_xmit(&ctx->ual_cookie, skb, repr);